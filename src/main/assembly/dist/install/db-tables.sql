bfa5de1ed73380e2a885d75f464a55e545fc3bd3
Jan van Mansum <janvanmansum@users.noreply.github.com>
2021-07-08 17:30:46 +0200

    Syntax error in db script

--- src/main/assembly/dist/install/db-tables.sql
+++ src/main/assembly/dist/install/db-tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS deposit_properties (
     depositor_user_id VARCHAR(50) NOT NULL,
     datamanager VARCHAR(50),
     storage_size_in_bytes bigint NOT NULL,
-    location VARCHAR(100) NOT NULL
+    location VARCHAR(100) NOT NULL,
     PRIMARY KEY (uuid));
 
 GRANT INSERT, SELECT, UPDATE, DELETE ON deposit_properties TO easy_manage_deposit;
